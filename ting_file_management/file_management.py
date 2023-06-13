782d35346b313cf3233c75f0c655a9cbe0f9b889
HugoRamosC <hugor.castilho@gmail.com>
2023-06-13 17:43:38 -0300

    feat: file management - req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,23 @@
+import sys
+
+
+class FileExtensionError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        with open(path_file, mode="r") as file:
+            if str(path_file).split('.')[-1].lower() != "txt":
+                raise FileExtensionError("Formato inválido")
+
+            # lines = [line.strip('\n') for line in file]
+            return file.read().splitlines()
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return
+    except FileExtensionError as e:
+        print(e.message, file=sys.stderr)
+        return
