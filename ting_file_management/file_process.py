b46e53b680c598cc491ba0b7de63cf1aa4ea49d6
HugoRamosC <hugor.castilho@gmail.com>
2023-06-13 19:47:25 -0300

    feat: process function - req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+    if len(instance) != 0 and path_file in instance.queue:
+        return
+
+    content_list = txt_importer(path_file)
+    file_dict = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(content_list),
+        'linhas_do_arquivo': content_list,
+    }
+    instance.enqueue(file_dict['nome_do_arquivo'])
+    print(file_dict, file=sys.stdout)
 
 
 def remove(instance):
